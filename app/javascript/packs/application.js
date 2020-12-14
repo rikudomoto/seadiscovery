321980e0e5ffa1a336685476171b5ea85009cb33
rikudomoto <73264978+rikudomoto@users.noreply.github.com>
2020-12-14 17:35:02 +0900

    修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("../map")
+
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
