29f0f3faa32b1c929baa7c24e867444ee03c5899
rikudomoto <73264978+rikudomoto@users.noreply.github.com>
2020-12-21 17:36:19 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'seas#index'
   devise_for :users
   resources :seas, only: [:new,:create] 
-  resources :homes, only:[:index,:show,:edit,:update,:destroy]
+  resources :homes, only:[:index,:show,:edit,:update,:destroy] 
 end
